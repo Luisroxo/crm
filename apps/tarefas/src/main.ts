8c22e23af3994eeebe86d39b2513fdac3a94fc26
Luis Roxo <luis_roxo@hub360plus.com.br>
2025-09-18 17:36:56 -0300

    feat: adiciona logs estruturados com Winston em todos os serviços e atualiza documentação

--- apps/tarefas/src/main.ts
+++ apps/tarefas/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json()
+          ),
+        }),
+      ],
+    }),
+  });
   await app.listen(3005);
 }
 bootstrap();
